e84420946827787046f493b6a41d61e504f33c46
Tran Quoc Phi <>
2023-03-05 19:06:25 +0700

    create new source

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { ROUTERS } from './router';
+import { Route } from 'react-router-dom';
+import AuthRouter from './components/AuthRouter/AuthRouter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthRouter router={ROUTERS}>
+      {(routes = []) => routes.map((item,key) => <Route key={key} path={item.path} element={item.element}/>)}
+    </AuthRouter>
   );
 }
 
